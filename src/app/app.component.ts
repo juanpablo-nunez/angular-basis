9c0eb0abc1910186ac5193cdf084f4f4d25d776b
Juan Pablo Nuñez <juanpanm2409@gmail.com>
2023-08-16 12:20:16 -0400

    Increase, decrease and reset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  private value: number = 10;
+  public title: string = 'Buenas a todos';
+  public counter: number = this.value;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+  resetCounter(): void {
+    this.counter = this.value;
+  }
 }
